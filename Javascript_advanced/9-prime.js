2c622f41bdfe97ee83c36b148980952f5c88d000
Elizbeh <eliz01_01@yahoo.com>
2023-05-10 14:52:50 +0200

    9. Prime numbers & timing execution

---
+++ Javascript_advanced/9-prime.js
@@ -0,0 +1,31 @@
+let countPrimeNumbers = function() {
+  var timeBegin = performance.now();
+
+  let count = 0;
+  for (let i = 2; i <=100; i++) {
+    if(isPrime(i)) {
+      count++;
+    }
+  }
+
+  const timeEnd = performance.now();
+  const executionTime = timeEnd - timeBegin;
+
+  console.log(`Execution time of printing countPrimeNumbers was ${executionTime} milliseconds.`);
+  return count;
+}
+
+function isPrime(number) {
+  if (number < 2) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+countPrimeNumbers();
